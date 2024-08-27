8400267a0023a1f879ad6eeb32e30e337d680558
LeoMasago <leo.masago@etec.gov.sp.br>
2024-08-26 22:35:01 -0300

    Adicionando modo escuro

---
+++ script.js
@@ -0,0 +1,6 @@
+function darkMode() {
+    var element = document.body;
+    var button = document.getElementById('btn');
+    element.classList.toggle("dark-mode");
+    button.classList.toggle("dark-mode");
+}
